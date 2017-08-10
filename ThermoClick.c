09b5e88c4b7f4beacceabc54768234cf8eeb7cce
kirkewing <1704846+kirkewing@users.noreply.github.com>
2017-08-10 10:03:57 -0700

    More testing

--- ThermoClick.c
+++ ThermoClick.c
@@ -88,16 +88,14 @@ int relay4_click_set_relay_state(uint8_t mikrobus_index, uint8_t relay, uint8_t 
     uint8_t gpio_pin = 0;
     int ret = 0;
 
-    gpio_get_pin(mikrobus_index, TYPE_PWM, &gpio_pin);
-	
     ret = find_gpio(mikrobus_index, relay);
     if (ret < 0)
         return -1;
 
     gpio_pin = (uint8_t)ret;
     
-    if (gpio_init(gpio_pin) < 0
-    ||  gpio_set_direction(gpio_pin, GPIO_OUTPUT) < 0
+    if (gpio_initw(gpio_pin) < 0
+    ||  gpio_set_directionw(gpio_pin, GPIO_OUTPUT) < 0
     ||  gpio_set_value(gpio_pin, !!state) < 0)
         return -1;
 
