6bfca4e960996586d7e805621919ead9052fe513
kirkewing <1704846+kirkewing@users.noreply.github.com>
2017-08-10 10:36:10 -0700

    Better testing stuff

--- ThermoClick.c
+++ ThermoClick.c
@@ -189,29 +189,35 @@ int main()
 	spi_init();			// initialize the SPI bus                                                
 	spi_select_bus(MIKROBUS_1);     // Ensure you have Mikrobus_1 selected for the SPI port                                                  
         
+	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_1);
+	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_2);
+	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_4);
+	printf("Disable all relays\n");
+	sleep(5);
+		
 	relay4_click_enable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_1);
 	printf("Enable Relay 1\n");
-	sleep(1);
+	sleep(5);
 	
 	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_1);
 	printf("Disable Relay 1\n");
-	sleep(1);
+	sleep(5);
 	
 	relay4_click_enable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_2);
 	printf("Enable Relay 2\n");
-	sleep(1);
+	sleep(5);
 	
 	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_2);
 	printf("Disable Relay 2\n");
-	sleep(1);
+	sleep(5);
 	
 	relay4_click_enable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_4);
 	printf("Enable Relay 4\n");
-	sleep(1);
+	sleep(5);
 	
 	relay4_click_disable_relay (MIKROBUS_2, RELAY4_CLICK_RELAY_4);
 	printf("Disable Relay 4\n");
-	sleep(1);
+	sleep(5);
 	
 		
         while(1) {                                                                        
